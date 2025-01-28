7aefa5fb4039004d6e80d1becb16937ddea78fb4
uolmez <umit.olmez@orioninc.com>
2025-01-28 13:32:54 +0300

    spring 13 done

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/entity/Movie.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.MovieState;
+import com.cydeo.enums.MovieType;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Movie extends BaseEntity{
+
+    private String name;
+    //@Column(columnDefinition = "DATE")
+    private BigDecimal price;
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "text")
+    private String summary;
+    @Enumerated(EnumType.STRING)
+    private MovieType type;
+    @Enumerated(EnumType.STRING)
+    private MovieState state;
+
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genreList;
+}
